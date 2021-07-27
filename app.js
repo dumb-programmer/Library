74823c6afe603dd2ab0083860cd99d32611b7c2e
dumb-programmer <asadkhan6192@gmail.com>
2021-07-27 22:28:24 +0500

    Improve UI and Make it Functional

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ function addBookToLibrary() {
     let read = document.querySelector('#read');;
     let book = new Book(title.value, author.value, pages.value, read.value);
     myLibrary.push(book);
-    insertBookInTheTable(myLibrary[myLibrary.length - 1])
+    insertBookInTable(myLibrary[myLibrary.length - 1])
 }
 
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; ++i) {
         let element = myLibrary[i];
-        insertBookInTheTable(element);
+        insertBookInTable(element);
     }
 }
 
-function insertBookInTheTable(bookObject) {
+function insertBookInTable(bookObject) {
     const tbody = document.querySelector('tbody');
     const tr = document.createElement('tr');
     tbody.append(tr);
@@ -43,8 +43,51 @@ function insertBookInTheTable(bookObject) {
     }
 }
 
-const btn = document.querySelector('button');
+const appendFormBtn = document.querySelector('#add-book');
+const okBtn = document.querySelector("#ok-btn");
 
-btn.addEventListener('click', addBookToLibrary)
+appendFormBtn.addEventListener('click', () => {
+    const form = document.querySelector("form")
+    if (appendFormBtn.textContent === "x") {
+        form.style.display = "none";
+        appendFormBtn.style.backgroundColor = "rgba(55, 204, 9, 0.933)";
+        appendFormBtn.textContent = "+";
+        appendFormBtn.style = hoverStyle;
+    }
+    else {
+        form.style.display = "block";
+        appendFormBtn.style.backgroundColor = "red";
+        appendFormBtn.textContent = "x";
+        appendFormBtn.style.hover.backgroundImage = "linear-gradient(#ff6a8a, #ff0037)";
+    }
+})
+
+appendFormBtn.addEventListener('mouseenter',() => {
+    if(appendFormBtn.textContent === "x"){
+        appendFormBtn.style.backgroundImage = "linear-gradient(#ff6a8a, #ff0037)";
+    }
+    else{
+        appendFormBtn.style.backgroundImage = "linear-gradient(#84fc03, #03fc14)";
+    }
+})
+
+appendFormBtn.addEventListener('mouseleave', () => {
+    appendFormBtn.style.backgroundImage = "none";
+    if(appendFormBtn.textContent === "x"){
+        appendFormBtn.style.backgroundColor = "red";
+    }
+    else{
+        appendFormBtn.style.backgroundColor = "rgba(55, 204, 9, 0.933)";
+    }
+})
+
+okBtn.addEventListener('click', () => {
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const read = document.querySelector("#read");
+    const book = new Book(title.value, author.value, pages.value, read.value);
+    insertBookInTable(book);
+})
 
 displayBooks()
